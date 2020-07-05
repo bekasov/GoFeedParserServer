975fd6d668e61d7fca292f2ecef4a6289ef1a6ba
Gennady Bekasov <bekasov.g@gmail.com>
2020-07-05 11:00:14 +0300

    Add more speed and readability

--- main.go
+++ main.go
@@ -22,10 +22,86 @@ type InputParams struct {
 	SortOutput bool
 }
 
+func main() {
+	http.HandleFunc("/", httpHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
 func httpHandler(w http.ResponseWriter, r *http.Request) {
 	var inputParams InputParams = GetInputParams(r.URL.Query())
 	response := GetResponse(RssFeeds, inputParams)
 	fmt.Fprintf(w, response)
+}
+
+func GetResponse(rssFeeds []string, params InputParams) string {
+	var resultChan <-chan ResultData = GetResultChan(rssFeeds, params)
+
+	var responseBuilder strings.Builder
+	responseBuilder.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+	responseBuilder.WriteString("<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">")
+	responseBuilder.WriteString("<channel>")
+	responseBuilder.WriteString("<title>Feed parser (search key is " + params.SearchString + ")</title>")
+	//responseBuilder.WriteString("<description>Feed parser</description>")
+	//responseBuilder.WriteString("<link>http://localhost/rss</link>")
+
+	if params.SortOutput {
+		var allFeeds *ResultDataArray = new(ResultDataArray)
+		for itemData := range resultChan {
+			allFeeds.Add(itemData)
+		}
+		sort.Sort(allFeeds)
+		for _, itemData := range *allFeeds {
+			responseBuilder.WriteString(*itemData.Xml)
+		}
+	} else {
+		for itemData := range resultChan {
+			responseBuilder.WriteString(*itemData.Xml)
+		}
+	}
+
+	responseBuilder.WriteString("</channel>")
+	responseBuilder.WriteString("</rss>")
+
+	return responseBuilder.String()
+}
+
+func GetResultChan(rssFeeds []string, params InputParams) <-chan ResultData {
+	var httpContentChan <-chan WebPageData = GetHttpContentChan(rssFeeds)
+	var result chan ResultData = make(chan ResultData)
+	var processWaitHandlers []*sync.WaitGroup
+	go func() {
+		for webPageData := range httpContentChan {
+			var processWaitHandler sync.WaitGroup = sync.WaitGroup{}
+			processWaitHandler.Add(1)
+			go ParseFeedItems(webPageData.Content, params, &processWaitHandler, result)
+			processWaitHandlers = append(processWaitHandlers, &processWaitHandler)
+		}
+
+		go func() {
+			for _, processWaitHandlerLoc := range processWaitHandlers {
+				processWaitHandlerLoc.Wait()
+			}
+			close(result)
+		}()
+	}()
+
+	return result
+}
+
+func GetHttpContentChan(rssFeeds []string) <-chan WebPageData {
+	var downloadWaitHandler sync.WaitGroup
+	var result = make(chan WebPageData)
+	for _, rssFeed := range rssFeeds {
+		downloadWaitHandler.Add(1)
+		go GetHttpContent(rssFeed, &downloadWaitHandler, result)
+	}
+
+	go func() {
+		downloadWaitHandler.Wait()
+		close(result)
+	}()
+
+	return result
 }
 
 func GetInputParams(getValuesPairs url.Values) InputParams {
@@ -46,68 +122,4 @@ func GetInputParams(getValuesPairs url.Values) InputParams {
 		CaseSensitive: caseSensitive,
 		SortOutput:    sortOutput,
 	}
-}
-
-func main() {
-	http.HandleFunc("/", httpHandler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func GetResponse(rssFeeds []string, params InputParams) string {
-	var downloadWaitHandler sync.WaitGroup
-	var httpDataChan = make(chan WebPageData)
-	for _, rssFeed := range rssFeeds {
-		downloadWaitHandler.Add(1)
-		go GetHttpContent(rssFeed, &downloadWaitHandler, httpDataChan)
-	}
-
-	var processResultChan chan ResultData = make(chan ResultData)
-
-	go func() { // !
-		downloadWaitHandler.Wait()
-		close(httpDataChan)
-	}()
-
-	var processWaitHandlers []*sync.WaitGroup
-	for webPageData := range httpDataChan {
-		var processWaitHandler sync.WaitGroup = *new(sync.WaitGroup)
-		processWaitHandler.Add(1)
-		go ParseFeedItems(webPageData.Content, params, &processWaitHandler, processResultChan)
-		processWaitHandlers = append(processWaitHandlers, &processWaitHandler)
-	}
-
-	var response strings.Builder
-	response.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
-	response.WriteString("<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">")
-	response.WriteString("<channel>")
-	response.WriteString("<title>Feed parser (search key is " + params.SearchString + ")</title>")
-	//response.WriteString("<description>Feed parser</description>")
-	//response.WriteString("<link>http://localhost/rss</link>")
-
-	go func() { // !
-		for _, processWaitHandlerLoc := range processWaitHandlers {
-			processWaitHandlerLoc.Wait()
-		}
-		close(processResultChan)
-	}()
-
-	if params.SortOutput {
-		var allFeeds *ResultDataArray = new(ResultDataArray)
-		for itemData := range processResultChan {
-			allFeeds.Add(itemData)
-		}
-		sort.Sort(allFeeds)
-		for _, itemData := range *allFeeds {
-			response.WriteString(*itemData.Xml)
-		}
-	} else {
-		for itemData := range processResultChan {
-			response.WriteString(*itemData.Xml)
-		}
-	}
-
-	response.WriteString("</channel>")
-	response.WriteString("</rss>")
-
-	return response.String()
 }
